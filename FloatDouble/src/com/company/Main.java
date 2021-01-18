d4f93406873f9af23f41411e26b243cb490c2345
Shankhadeep1234 <shankhadeepraj@gmail.com>
2021-01-18 16:31:29 +0530

    float_double

---
+++ FloatDouble/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("float min Value :" + myMinFloatValue);
+        System.out.println("float max value :" + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("double min Value :" + myMinDoubleValue);
+        System.out.println("double max value :" + myMaxDoubleValue);
+
+    }
+}
