edc9b25fc8c744d69fbad57bea7a32036b5ce3e7
Shankhadeep1234 <shankhadeepraj@gmail.com>
2021-01-19 11:53:29 +0530

    if_then & operators & methods

---
+++ if_then_and_operators/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("He is not an alien");
+        } else {
+            System.out.println("Yes he is an alien...!!");
+        }
+
+
+        int topScore = 100;
+        if (topScore == 100) {  //< > ! == || &&
+            System.out.println("yeeaahh.. you got the high score");
+        } else {
+            System.out.println("better luck next time");
+        }
+    }
+}
